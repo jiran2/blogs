6345bb8166ccc8b2030aa6454bcff9a190bccf89
ranji <ranji@iflytek.com>
2023-05-22 14:59:19 +0800

    auto commit at 2023/05/22 周一 14:59:19.55

--- blog_push.bat
+++ blog_push.bat
@@ -4,6 +4,9 @@ if "%1"=="h" goto begin
 start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
 :begin
 
+del blog_push.log
+del blog_error.log
+
 D:
 cd /d D:\blogs
 
@@ -16,7 +19,7 @@ git pull 2>>blog_error.log
   
 git push 2>>blog_error.log
  
-echo "%now%" > blog_push.log
+echo "%now%" >> blog_push.log
 
 set /a "pause_time=%RANDOM% %% 17 + 1"
 timeout /nobreak /t %pause_time% 2>>blog_error.log

7027d5e58632d0f685d89c31881e4e7b3f5ff8fe
ranji <ranji@iflytek.com>
2023-05-22 15:05:24 +0800

    auto commit at 2023/05/22 周一 15:05:24.21

--- blog_push.bat
+++ blog_push.bat
@@ -1,11 +1,8 @@
 @echo off
 
-if "%1"=="h" goto begin
-start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
 :begin
 
-del blog_push.log
-del blog_error.log
+echo. > blog_push.log
 
 set now=%date% %time%
 
@@ -15,16 +12,17 @@ cd /d D:\blogs
 echo "%now%" >> blog_push.log
 git add * 2>>blog_push.log
 
-set now=%date% %time%
-git commit -m "auto commit at %now%" 2>>blog_error.log
-
-git pull 2>>blog_error.log
-  
-git push 2>>blog_error.log
- 
 echo "%now%" >> blog_push.log
+git commit -m "auto commit at %now%" 2>>blog_push.log
 
+echo "%now%" >> blog_push.log
+git pull 2>>blog_push.log
+  
+echo "%now%" >> blog_push.log
+git push 2>>blog_push.log
+
+echo "%now%" >> blog_push.log
 set /a "pause_time=%RANDOM% %% 17 + 1"
-timeout /nobreak /t %pause_time% 2>>blog_error.log
+timeout /nobreak /t %pause_time% 2>>blog_push.log
 
 goto begin
